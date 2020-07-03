3b71e5b18e353db5184699d0d30ba579e0756c29
Demian Rosas <drosas@intouchhealth.com>
2020-07-03 13:55:36 -0700

    Pruebas

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
 // replace these values with those generated in your TokBox Account
 var apiKey = "46535332";
-var sessionId = "1_MX40NjUzNTMzMn5-MTU4MzkzMzA0Njk4NH5nUmtjVzBxM055d0NzTzdHUGg1eFJDL2V-UH4";
+var sessionId =
+  "1_MX40NjUzNTMzMn5-MTU4MzkzMzA0Njk4NH5nUmtjVzBxM055d0NzTzdHUGg1eFJDL2V-UH4";
 var publisher;
 // (optional) add server code here
 //initializeSession();
-
-
 
 // Handling all of our errors here by alerting them
 function handleError(error) {
@@ -18,25 +17,38 @@ function initializeSession() {
   var session = OT.initSession(apiKey, sessionId);
 
   // Subscribe to a newly created stream
-  session.on('streamCreated', function(event) {
-  session.subscribe(event.stream, 'subscriber', {
-      insertMode: 'append',
-      width: '100%',
-      height: '100%'
-    }, handleError);
+  session.on("streamCreated", function (event) {
+    session.subscribe(
+      event.stream,
+      "subscriber",
+      {
+        insertMode: "append",
+        width: "100%",
+        height: "100%",
+      },
+      handleError
+    );
   });
 
   // Create a publisher
-  publisher = OT.initPublisher('publisher', {
-    insertMode: 'append',
-    width: '100%',
-    height: '100%'
-  }, handleError);
+  publisher = OT.initPublisher(
+    "publisher",
+    {
+      insertMode: "append",
+      width: "100%",
+      height: "100%",
+      videoSource:
+        "74cf348252c4411694249d617da13a95b1ded9310dd338e4e43dd8a9f7ecfdfb",
+    },
+    handleError
+  );
+
+  publisher.publishVideo(true);
 
   // Connect to the session
   var tokenInput = document.getElementById("token");
   var token = tokenInput.value;
-  session.connect(token, function(error) {
+  session.connect(token, function (error) {
     // If the connection is successful, publish to the session
     if (error) {
       handleError(error);
@@ -47,14 +59,14 @@ function initializeSession() {
 }
 
 function toggleCamera() {
-  publisher.cycleVideo()
+  publisher.cycleVideo();
 }
 
 function publishVideo(hasVideo) {
-  publisher.publishVideo(hasVideo)
+  publisher.publishVideo(hasVideo);
 }
 
 function toggleVideo() {
-  var hasVideo = publisher.stream.hasVideo
-  publishVideo(!hasVideo)
+  var hasVideo = publisher.stream.hasVideo;
+  publishVideo(!hasVideo);
 }
